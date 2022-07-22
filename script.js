a39d288575987c8c5c7d4d90a070b5759e447532
Raniya Abdalla <58631480+raniyaabdalla@users.noreply.github.com>
2022-07-22 17:34:52 -0400

    Return

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", () => {
+    const fetchPromise = fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+    fetchPromise.then(function(response){
+        const jsonPromise = response.json();
+           jsonPromise.then(function(json) {
+        const astlist = document.getElementById("container");
+        for(let i=0;i<jdon.length;i++){
+            astlist.innerHTML+=`
+            <div class"astronauts">
+              <div class = "bio">
+                <h3>${json[i].firstName} ${json[i].lateName}</h3>
+                <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li>active: ${json[i].active}</li>
+                    <li>HSkills: ${json[i].skills}</li>
+                </ul>
+                </div>
+                <img class ='avatar' src = ${json[i].picture}>
+            </div>`;    
+        }
+     })
+})
+})
